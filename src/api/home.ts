a4f02c79ff7d5a6cbd9581de385c02a06f43c8ae
CrazyQzq <13186061107@163.com>
2022-07-16 21:16:20 +0800

    init commit

---
+++ src/api/home.ts
@@ -0,0 +1,22 @@
+
+import request from '@src/request'
+
+/**
+ * 查询banners
+ * @returns
+ */
+export const getBanners = () => {
+  return request.get({
+    url: '/system/home/banners'
+  })
+}
+
+/**
+ * 获取公告
+ * @returns
+ */
+export const getAnnouncement = () => {
+  return request.get({
+    url: '/system/home/announcement'
+  })
+}
---
+++ src/api/order.ts
@@ -0,0 +1,88 @@
+import request from '@src/request'
+
+/**
+ * 获取订单列表
+ * @param params
+ */
+export const getOrderList = (params: object) => {
+  return request.get({
+    url: '/order/list',
+    params: params
+  })
+}
+
+/**
+ * 保存订单
+ * @param params
+ */
+export const saveOrder = (params: object) => {
+  return request.post({
+    url: '/order/saveOrder',
+    data: params,
+    headers: { 'Content-Type': 'application/json' }
+  })
+}
+
+/**
+ * 获取购物车列表
+ * @returns
+ */
+export const getShopingCartItems = (params: object) => {
+  return request.get({
+    url: '/order/cart/list',
+    params: params
+  })
+}
+
+/**
+ * 添加购物车
+ * @param params
+ */
+export const addCartItem = (params: object) => {
+  return request.put({
+    url: '/order/cart/add',
+    data: params,
+    headers: { 'Content-Type': 'application/json' }
+  })
+}
+
+/**
+ * 修改购物车商品数量
+ * @param params
+ */
+export const updateCartItemNum = (params: object) => {
+  return request.post({
+    url: '/order/cart/update',
+    params: params
+  })
+}
+
+/**
+ * 删除购物车商品
+ * @param params
+ */
+export const deleteCartItem = (params: object) => {
+  return request.delete({
+    url: '/order/cart/delete',
+    params: params
+  })
+}
+
+/**
+ * 查询热卖商品
+ */
+export const hotSales = () => {
+  return request.get({
+    url: '/order/hotSales'
+  })
+}
+
+/**
+ * 查询订单各种状态数量
+ */
+export const getStatusCount = (params: object) => {
+  return request.get({
+    url: '/order/getStatusCount',
+    params: params
+  })
+}
