1d1c1874363a3117ad39d5be983d3145c78a1fba
CrazyQzq <13186061107@163.com>
2022-07-16 21:20:32 +0800

    init commit

---
+++ src/store/modules/user.ts
@@ -0,0 +1,143 @@
+import { VuexModule, Module, Action, Mutation, getModule } from 'vuex-module-decorators'
+import { login, logout, getUserInfo } from '@/api/users'
+import { getToken, setToken, removeToken } from '@/utils/cookies'
+import router, { resetRouter } from '@/router'
+import { PermissionModule } from './permission'
+import { TagsViewModule } from './tags-view'
+import store from '@/store'
+
+export interface IUserState {
+  userId: number
+  token: string
+  name: string
+  avatar: string
+  introduction: string
+  roles: string[]
+  email: string
+}
+
+@Module({ dynamic: true, store, name: 'user' })
+class User extends VuexModule implements IUserState {
+  public userId = -1
+  public token = getToken() || ''
+  public name = ''
+  public avatar = ''
+  public introduction = ''
+  public roles: string[] = []
+  public email = ''
+
+  @Mutation
+  private SET_ID(userId: number) {
+    this.userId = userId
+  }
+
+  @Mutation
+  private SET_TOKEN(token: string) {
+    this.token = token
+  }
+
+  @Mutation
+  private SET_NAME(name: string) {
+    this.name = name
+  }
+
+  @Mutation
+  private SET_AVATAR(avatar: string) {
+    this.avatar = avatar
+  }
+
+  @Mutation
+  private SET_INTRODUCTION(introduction: string) {
+    this.introduction = introduction
+  }
+
+  @Mutation
+  private SET_ROLES(roles: string[]) {
+    this.roles = roles
+  }
+
+  @Mutation
+  private SET_EMAIL(email: string) {
+    this.email = email
+  }
+
+  @Action
+  public async Login(userInfo: { username: string, password: string}) {
+    let { username, password } = userInfo
+    username = username.trim()
+    const { data } = await login({ username, password })
+    setToken(data.token.access_token)
+    this.SET_TOKEN(data.token.access_token)
+    const { userId, authorities, userName, avatar, remark, email } = data.user
+    this.SET_ID(userId)
+    this.SET_ROLES(authorities)
+    this.SET_NAME(userName)
+    this.SET_AVATAR(avatar)
+    this.SET_INTRODUCTION(remark)
+    this.SET_EMAIL(email)
+  }
+
+  @Action
+  public ResetToken() {
+    removeToken()
+    this.SET_TOKEN('')
+    this.SET_ROLES([])
+  }
+
+  @Action
+  public async GetUserInfo() {
+    if (this.token === '') {
+      throw Error('GetUserInfo: token is undefined!')
+    }
+    console.log('this.userId', this)
+    const { data } = await getUserInfo({ id: 1 })
+    if (!data) {
+      throw Error('Verification failed, please Login again.')
+    }
+    const { userName, avatar, remark, email } = data
+    // roles must be a non-empty array
+    // if (!roles || roles.length <= 0) {
+    //   throw Error('GetUserInfo: roles must be a non-null array!')
+    // }
+    this.SET_ROLES(['admin'])
+    this.SET_NAME(userName)
+    this.SET_AVATAR(avatar)
+    this.SET_INTRODUCTION(remark)
+    this.SET_EMAIL(email)
+  }
+
+  @Action
+  public async ChangeRoles(role: string) {
+    // Dynamically modify permissions
+    const token = role + '-token'
+    this.SET_TOKEN(token)
+    setToken(token)
+    await this.GetUserInfo()
+    resetRouter()
+    // Generate dynamic accessible routes based on roles
+    PermissionModule.GenerateRoutes(this.roles)
+    // Add generated routes
+    PermissionModule.dynamicRoutes.forEach(route => {
+      router.addRoute(route)
+    })
+    // Reset visited views and cached views
+    TagsViewModule.delAllViews()
+  }
+
+  @Action
+  public async LogOut() {
+    if (this.token === '') {
+      throw Error('LogOut: token is undefined!')
+    }
+    await logout()
+    removeToken()
+    resetRouter()
+
+    // Reset visited views and cached views
+    TagsViewModule.delAllViews()
+    this.SET_TOKEN('')
+    this.SET_ROLES([])
+  }
+}
+
+export const UserModule = getModule(User)
